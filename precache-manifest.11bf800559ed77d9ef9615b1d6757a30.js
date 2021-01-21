dec35e161a587c9a699863185ce4cbed038b9648
Travis CI <travisci@gmail.com>
2021-01-21 08:43:10 +0000

    Travis CI - Deploy to GitHub Pages

---
+++ precache-manifest.11bf800559ed77d9ef9615b1d6757a30.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3a422316e8f7476c86f5918a9119258",
+    "url": "/marvelact/index.html"
+  },
+  {
+    "revision": "c71cd3e12b935e37884e",
+    "url": "/marvelact/static/css/2.50b8cc23.chunk.css"
+  },
+  {
+    "revision": "c71cd3e12b935e37884e",
+    "url": "/marvelact/static/js/2.c0f78185.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/marvelact/static/js/2.c0f78185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef3ffba2be6d2769a44",
+    "url": "/marvelact/static/js/main.27b85d1f.chunk.js"
+  },
+  {
+    "revision": "c1ecad28f11723ad5bed",
+    "url": "/marvelact/static/js/runtime-main.fb5dbd8a.js"
+  },
+  {
+    "revision": "1fe88f27a66b844eaa6fa767cce7dbe3",
+    "url": "/marvelact/static/media/404.1fe88f27.jpg"
+  },
+  {
+    "revision": "e65ca72257aa9beeedee87f081c3483c",
+    "url": "/marvelact/static/media/load.e65ca722.gif"
+  },
+  {
+    "revision": "3934dccc67836989500997ec58fd7812",
+    "url": "/marvelact/static/media/marvel-background.3934dccc.png"
+  },
+  {
+    "revision": "1623b141ec62ef6e8787916bc7c424db",
+    "url": "/marvelact/static/media/shield-logo-white.1623b141.png"
+  }
+]);
